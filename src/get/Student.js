4b37f08b98700bd554bb5a13787df2bd5a35fba4
Mustafa Siam Ur Rafique <siam11651@outlook.com>
2022-08-13 05:53:19 +0600

    vallage na

---
+++ src/get/Student.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number} userId
+ * @param {string[]} select
+ */
+const GetStudentDetailsFromUserId = function(userId, select)
+{
+    if(!Array.isArray(select))
+    {
+        select = [];
+    }
+
+    let http = new XMLHttpRequest();
+
+    http.open('POST', '/', false);
+    http.setRequestHeader('Content-Type', 'application/json');
+
+    let data =
+    {
+        type: 'get-student-details',
+        user_id: parseInt(userId),
+        select: select
+    };
+
+    http.send(JSON.stringify(data));
+
+    if(http.readyState == 4 && http.status == 200)
+    {
+        let response = JSON.parse(http.responseText);
+
+        if(response.ok)
+        {
+            return response;
+        }
+        else
+        {
+            return null;
+        }
+    }
+    else
+    {
+        return null;
+    }
+};
---
+++ src/init/Student.js
@@ -0,0 +1,51 @@
+/**
+ * @type {string | null}
+ */
+let userId = localStorage.getItem('user_id');
+/**
+ * @type {string | null}
+ */
+let student = localStorage.getItem('student');
+let params = new URLSearchParams(location.search);
+
+if(userId == null || !params.has('user_id'))
+{
+    location.replace('index.html');
+}
+
+SetupNavBar(userId);
+
+let studentDetails = GetStudentDetailsFromUserId(userId, ['USER_ID', 'FULL_NAME', 'EMAIL', 'DATE_OF_BIRTH', 'DATE_OF_JOIN', 'BIO', 'RANK_POINT']);
+let studentPfp = document.getElementsByTagName('img').namedItem('student-pfp');
+let studentName = document.getElementsByTagName('h2').namedItem('student-name');
+let studentEmail = document.getElementsByTagName('h4').namedItem('student-email');
+let dateOfJoin = document.getElementsByTagName('h6').namedItem('date-of-join').getElementsByTagName('span').item(0);
+let dateOfBirth = document.getElementsByTagName('h6').namedItem('date-of-birth').getElementsByTagName('span').item(0);
+let rank = document.getElementsByTagName('h6').namedItem('rank-point').getElementsByTagName('span').item(0);
+// let interests = document.getElementsByTagName('h6').namedItem('interests').getElementsByTagName('span').item(0);
+let bioContainer = document.getElementsByTagName('div').namedItem('bio-container');
+let addBioButton = document.getElementsByTagName('a').namedItem('add-bio-button');
+
+if(studentDetails.BIO == '')
+{
+    addBioButton.parentNode.removeChild(bioContainer);
+
+    addBioButton.onclick = function()
+    {
+
+    };
+}
+else
+{
+    bioContainer.parentNode.removeChild(addBioButton);
+    
+    let bioCard = document.getElementsByTagName('div').namedItem('bio-card');
+    bioCard.textContent = studentDetails.BIO;
+}
+
+studentName.textContent = studentDetails.FULL_NAME;
+studentEmail.textContent = studentDetails.EMAIL;
+studentPfp.src = 'pfp/' + studentDetails.USER_ID + '.png';
+dateOfJoin.textContent = studentDetails.DATE_OF_JOIN;
+dateOfBirth.textContent = studentDetails.DATE_OF_BIRTH;
+rank.textContent = studentDetails.RANK_POINT;
