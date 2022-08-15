ff543c949196b7e9d26facba9cad83c98a12535a
Mustafa Siam Ur Rafique <siam11651@outlook.com>
2022-08-15 23:38:00 +0600

    about to implement file upload frontend

--- src/get/User.js
+++ src/get/User.js
@@ -38,3 +38,51 @@ const GetUserDetails = function(userId, select)
         return null;
     }
 };
+
+/**
+ * 
+ * @param {number} userId
+ * @param {string} fullName
+ * @param {string} email
+ * @param {string} bio
+ * @param {string} dateOfBirth
+ * @param {string} pfpBuffer
+ */
+const UpdateUserDetails = function(userId, fullName, email, password, bio, dateOfBirth)
+{
+    let http = new XMLHttpRequest();
+
+    http.open('POST', '/', false);
+    http.setRequestHeader('Content-Type', 'application/json');
+
+    let data = 
+    {
+        type: 'update-user-details',
+        user_id: parseInt(userId),
+        full_name: fullName,
+        email: email,
+        password: password,
+        bio: bio,
+        date_of_birth: dateOfBirth
+    };
+
+    http.send(JSON.stringify(data));
+
+    if(http.readyState == 4 && http.status == 200)
+    {
+        let response = JSON.parse(http.responseText);
+
+        if(response.ok)
+        {
+            return response;
+        }
+        else
+        {
+            return null;
+        }
+    }
+    else
+    {
+        return null;
+    }
+};
---
+++ src/init/Settings.js
@@ -0,0 +1,117 @@
+/**
+ * @type {string | null}
+ */
+let userId = localStorage.getItem('user_id');
+/**
+ * @type {string | null}
+ */
+let student = localStorage.getItem('student');
+
+if(userId == null)
+{
+    location.replace('index.html');
+}
+
+SetupNavBar(userId);
+
+let interestsContainer = document.getElementsByTagName('div').namedItem('interests-container');
+let specialitiesContainer = document.getElementsByTagName('div').namedItem('specialities-container');
+let addTeacherButton = document.getElementsByTagName('button').namedItem('teacher-button');
+let addStudentButton = document.getElementsByTagName('button').namedItem('student-button');
+
+if(student == 'true')
+{
+    specialitiesContainer.remove();
+    addStudentButton.remove();
+}
+else
+{
+    interestsContainer.remove();
+    addTeacherButton.remove();
+}
+
+let userDetails = GetUserDetails(userId, ['FULL_NAME', 'EMAIL', 'SECURITY_KEY', 'BIO', 'DATE_OF_BIRTH', 'PFP']);
+let fullName = document.getElementsByTagName('input').namedItem('full-name');
+let email = document.getElementsByTagName('input').namedItem('email');
+let password = document.getElementsByTagName('input').namedItem('password');
+let bio = document.getElementsByTagName('textarea').namedItem('bio');
+let pfp = document.getElementsByTagName('img').namedItem('pfp');
+let pfpFile = document.getElementsByTagName('input').namedItem('pfp-file');
+let dateOfBirth = document.getElementsByTagName('input').namedItem('date-of-birth');
+fullName.value = userDetails.FULL_NAME;
+email.value = userDetails.EMAIL;
+password.value = userDetails.SECURITY_KEY;
+bio.value = userDetails.BIO;
+dateOfBirth.valueAsDate = new Date(userDetails.DATE_OF_BIRTH);
+
+if(userDetails.PFP == 't')
+{
+    pfp.src = 'pfp/' + userId + '.png';
+}
+
+let saveButton = document.getElementsByTagName('button').namedItem('save-button');
+saveButton.onclick = async function()
+{
+    saveButton.setAttribute('disabled', '');
+
+    let fullNameValue = fullName.value;
+    let emailValue = email.value;
+    let passwordValue = password.value;
+    let bioValue = bio.value;
+    let dateValue = dateOfBirth.valueAsDate.getFullYear() + '-' + (dateOfBirth.valueAsDate.getMonth() + 1).toString() + '-' + dateOfBirth.valueAsDate.getDate();
+    let imageUploadSuccess = true;
+
+    if(pfpFile.files.length > 0)
+    {
+        let reader = new FileReader();
+
+        reader.readAsDataURL(pfpFile.files.item(0));
+
+        reader.onload = function(event)
+        {
+            let image = new Image();
+            image.src = event.target.result;
+            image.onload = function()
+            {
+                console.log(this.width + ' ' + this.height);
+            };
+        };
+    }
+
+    if(imageUploadSuccess)
+    {
+        let response = UpdateUserDetails(userId, fullNameValue, emailValue, passwordValue, bioValue, dateValue);
+
+        if(response.return == 0)
+        {
+            if(student == 'true')
+            {
+                location.href = 'student.html';
+            }
+            else
+            {
+                location.href = 'teacher.html';
+            }
+        }
+        else if(response.return == 1)
+        {
+            // empty full name
+        }
+        else if(response.return == 2)
+        {
+            // empty email
+        }
+        else if(response.return == 3)
+        {
+            //invalid email
+        }
+        else
+        {
+            // invalid password size
+        }
+    }
+    else
+    {
+
+    }
+}
