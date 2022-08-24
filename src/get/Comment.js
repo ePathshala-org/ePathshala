72553fbf32f4419dd2fe9d99376eb58bbaaffd4a
Mustafa Siam Ur Rafique <siam11651@outlook.com>
2022-08-24 22:54:43 +0600

    vallagana

--- src/get/Comment.js
+++ src/get/Comment.js
@@ -37,3 +37,138 @@ const GetCommentsFromContentId = function(contentId, select)
         return null;
     }
 };
+
+/**
+ * 
+ * @param {number} userId
+ * @param {number} contentId
+ */
+const PostComment = function(userId, contentId, description)
+{
+    let http = new XMLHttpRequest();
+
+    http.open('POST', '/', false);
+    http.setRequestHeader('Content-Type', 'application/json');
+
+    let data = 
+    {
+        type: 'post-comment',
+        user_id: parseInt(userId),
+        content_id: parseInt(contentId),
+        description: description
+    };
+
+    http.send(JSON.stringify(data));
+
+    if(http.readyState == 4)
+    {
+        if(http.status == 200)
+        {
+            return JSON.parse(http.responseText);
+        }
+        else
+        {
+            return {ok: false, error: http.status};
+        }
+    }
+}
+
+/**
+ * 
+ * @param {number} commentId 
+ */
+const DeleteComment = function(commentId)
+{
+    let http = new XMLHttpRequest();
+
+    http.open('POST', '/', false);
+    http.setRequestHeader('Content-Type', 'application/json');
+
+    let data = 
+    {
+        type: 'delete-comment',
+        comment_id: parseInt(commentId)
+    };
+
+    http.send(JSON.stringify(data));
+
+    if(http.readyState == 4)
+    {
+        if(http.status == 200)
+        {
+            return JSON.parse(http.responseText);
+        }
+        else
+        {
+            return {ok: false, error: http.status};
+        }
+    }
+};
+
+/**
+ * 
+ * @param {number} commentId 
+ * @param {number} rate 
+ */
+const UpdateCommentRate = function(commentId, rate)
+{
+    let http = new XMLHttpRequest();
+
+    http.open('POST', '/', false);
+    http.setRequestHeader('Content-Type', 'application/json');
+
+    let data = 
+    {
+        type: 'update-comment-rate',
+        comment_id: parseInt(commentId),
+        rate: parseInt(rate)
+    };
+
+    http.send(JSON.stringify(data));
+
+    if(http.readyState == 4)
+    {
+        if(http.status == 200)
+        {
+            return JSON.parse(http.responseText);
+        }
+        else
+        {
+            return {ok: false, error: http.status};
+        }
+    }
+};
+
+/**
+ * 
+ * @param {number} commentId
+ * @param {string} description
+ */
+const UpdateComment = function(commentId, description)
+{
+    let http = new XMLHttpRequest();
+
+    http.open('POST', '/', false);
+    http.setRequestHeader('Content-Type', 'application/json');
+
+    let data = 
+    {
+        type: 'update-comment',
+        comment_id: parseInt(commentId),
+        description: description
+    };
+
+    http.send(JSON.stringify(data));
+
+    if(http.readyState == 4)
+    {
+        if(http.status == 200)
+        {
+            return JSON.parse(http.responseText);
+        }
+        else
+        {
+            return {ok: false, error: http.status};
+        }
+    }
+};
