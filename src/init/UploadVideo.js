bd81c7566e18d3e1cfc51f961cc3b7c8a2f94841
Mustafa Siam Ur Rafique <siam11651@outlook.com>
2022-08-26 08:00:46 +0600

    upload video page done... ig...

--- src/init/UploadVideo.js
+++ src/init/UploadVideo.js
@@ -29,33 +29,57 @@ if(courseDetails.CREATOR_ID != userId)
     location.replace('teacher.html');
 }
 
+let title = document.getElementsByTagName('input').namedItem('video-title');
+let description = document.getElementsByTagName('textarea').namedItem('video-description');
 let fileSelect = document.getElementsByTagName('input').namedItem('video-file');
 let modalElement = document.getElementsByTagName('div').namedItem('upload-state-modal');
 let uploadStateModal = new bootstrap.Modal(modalElement);
 let progressBar = document.getElementsByTagName('div').namedItem('upload-progress-bar');
-
+let progressValue = document.getElementsByTagName('span').namedItem('upload-progress-value');
+let uploadCancelButton = document.getElementsByTagName('button').namedItem('cancel-video-upload-button');
 let uploadForm = document.getElementsByTagName('form').item(0);
 uploadForm.onsubmit = function()
 {
-    uploadStateModal.show();
-
-    let fileFormData = new FormData();
-
-    fileFormData.append('file', fileSelect.files.item(0));
-
-    let http = new XMLHttpRequest();
-    
-    http.open('POST', '/server-test', false);
-    http.setRequestHeader('Incoming', 'video-file');
-
-    http.upload.onprogress = function(ev)
+    if(uploadForm.checkValidity())
     {
-        console.log('again hehe ' + ev.loaded + ' ' + ev.total);
-        console.log(String(parseInt((ev.loaded / ev.total) * 100)));
-        progressBar.style.width = String(parseInt((ev.loaded / ev.total) * 100)) + '%';
-    };
+        uploadStateModal.show();
 
-    http.send(fileFormData);
+        let fileFormData = new FormData();
+
+        fileFormData.append('file', fileSelect.files.item(0));
+
+        let http = new XMLHttpRequest();
+        
+        http.open('POST', '/');
+        http.setRequestHeader('incoming', 'video-file');
+        http.setRequestHeader('course_id', courseId);
+        http.setRequestHeader('title', title.value);
+        http.setRequestHeader('description', description.value);
+
+        http.upload.onprogress = function(ev)
+        {
+            let value = String(parseInt((ev.loaded / ev.total) * 100)) + '%';
+            progressBar.style.width = value;
+            progressValue.textContent = value;
+        };
+
+        http.onreadystatechange = function(ev)
+        {
+            if(this.readyState == 4)
+            {
+                let response = JSON.parse(this.responseText);
+
+                if(response.ok)
+                {
+                    uploadStateModal.hide();
+                    
+                    location.href = 'customizecourse.html?course_id=' + courseId;
+                }
+            }
+        }
+
+        http.send(fileFormData);
+    }
 
     return false;
 }
